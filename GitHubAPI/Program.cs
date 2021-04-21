675ddfc2a3ab854c1f7ffc3d009af8fefba1de42
kamilxkruk@gmail.com <kamilxkruk@gmail.com>
2021-04-21 22:25:27 +0200

    WiP - Implement IHttpClientFactory to call GitHub API

--- GitHubAPI/Program.cs
+++ GitHubAPI/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Net.Http;
 
 namespace GitHubAPI
 {
@@ -6,7 +11,28 @@ namespace GitHubAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configurationBuilder = new ConfigurationBuilder();
+            BuildConfig(configurationBuilder);
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((contect, services) =>
+                {
+                    services.AddHttpClient(name: "GitHub", client =>
+                      {
+                          client.BaseAddress = new Uri("https://api.github.com");
+                          client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                          client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactoryExample");
+                      });
+                })
+                .Build();
+        }
+
+        static void BuildConfig(IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
         }
     }
 }
---
+++ GitHubAPI/Services/GitHubService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubAPI.Services
+{
+    
+    class GitHubService
+    {
+        public IHttpClientFactory HttpClientFactory { get; set; }
+
+        public bool CheckGithubAPIConnection()
+        {
+            var httpClient = HttpClientFactory.CreateClient("GitHub");
+
+            //TODO Implement API call
+
+            return false;
+        }
+    
+    }
+}
